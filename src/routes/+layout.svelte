cd37e0dc97a4e57a67556f8e9e71f2546d28573b
ezlosswm <ezlosswm@gmail.com>
2026-03-07 10:19:19 -0600

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/logo.svg';
 	import { navigating } from '$app/state';
 	import { expoOut } from 'svelte/easing';
 	import { fly } from 'svelte/transition';
